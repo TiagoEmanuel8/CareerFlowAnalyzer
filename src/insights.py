d5520f2d5fa483e850fb8c6e6cdc2d6c27f643c3
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-01-27 21:38:42 -0300

    req2: concluido

--- src/insights.py
+++ src/insights.py
@@ -43,7 +43,11 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    jobs_filter = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            jobs_filter.append(job)
+    return jobs_filter
 
 
 def get_unique_industries(path):
