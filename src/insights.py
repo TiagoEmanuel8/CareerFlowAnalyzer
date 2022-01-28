5fe7d2b8cbc7630fe1a498db853ebfcff7320480
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-01-28 17:47:02 -0300

    req7: concluido

--- src/insights.py
+++ src/insights.py
@@ -83,7 +83,11 @@ def filter_by_industry(jobs, industry):
     list
         List of jobs with provided industry
     """
-    return []
+    job_filter_by_ind = []
+    for job in jobs:
+        if job["industry"] == industry:
+            job_filter_by_ind.append(job)
+    return job_filter_by_ind
 
 
 def get_max_salary(path):
