f7542242a79dfe17d88edc4ff2b4c0d5063a94f8
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2022-01-28 18:44:54 -0300

    req10.1 e 10.2: concluidos

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,37 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    """Mock de dados"""
+    return [
+        {
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2022-01-28",
+        },
+        {
+            "min_salary": 2000,
+            "max_salary": 3000,
+            "date_posted": "2022-01-28",
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 1500,
+            "date_posted": "2022-01-28",
+        },
+        {
+            "min_salary": 500,
+            "max_salary": 1000,
+            "date_posted": "2022-01-28",
+        },
+    ]
+
+
+def test_sort_by_criteria(jobs):
+    sort_by(jobs, "min_salary")
+    min_salary = []
+    for job in jobs:
+        min_salary.append(job["min_salary"])
+    assert min_salary == [500, 800, 1000, 2000]
